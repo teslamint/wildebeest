d824841b222f12f2855f3e945a73e8c53137ea1f
Jaehoon You <teslamint@gmail.com>
2025-05-06 19:53:40 +0900

    webfinger: response 410

--- functions/.well-known/webfinger.ts
+++ functions/.well-known/webfinger.ts
@@ -1,11 +1,9 @@
 // https://www.rfc-editor.org/rfc/rfc7033
 
-import { getActorById } from 'wildebeest/backend/src/activitypub/actors'
 import type { Env } from 'wildebeest/backend/src/types'
-import type { WebFingerResponse } from 'wildebeest/backend/src/webfinger'
 import { type Database, getDatabase } from 'wildebeest/backend/src/database'
-import { handleToAcct, isLocalHandle, parseHandle } from 'wildebeest/backend/src/utils/handle'
-import { getUserId, isLocalAccount } from 'wildebeest/backend/src/accounts'
+import { isLocalHandle, parseHandle } from 'wildebeest/backend/src/utils/handle'
+import { isLocalAccount } from 'wildebeest/backend/src/accounts'
 
 export const onRequest: PagesFunction<Env, any> = async ({ request, env }) => {
 	return handleRequest(request, await getDatabase(env))
@@ -37,24 +35,5 @@ export async function handleRequest(request: Request, db: Database): Promise<Res
 		return new Response('', { status: 404 })
 	}
 
-	const actor = await getActorById(db, getUserId(domain, handle))
-	if (actor === null) {
-		return new Response('', { status: 404 })
-	}
-
-	const jsonLink = actor.id.toString()
-
-	const res: WebFingerResponse = {
-		subject: `acct:${handleToAcct(handle)}`,
-		aliases: [jsonLink],
-		links: [
-			{
-				rel: 'self',
-				type: 'application/activity+json',
-				href: jsonLink,
-			},
-		],
-	}
-
-	return new Response(JSON.stringify(res), { headers })
+	return new Response(null, { status: 410, statusText: 'Gone', headers })
 }
